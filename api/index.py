8ad3ed7a0c28434d48d486708a810fb60d2adbaa
gautamkumar30 <gautamkumarganesan@gmail.com>
2024-10-05 14:19:17 +0530

    integrate flask

---
+++ api/index.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import jsonify
+import json
+from flask.helpers import send_file
+
+
+
+app = Flask(__name__)
+
+@app.route("/api/python")
+def hello_world():
+    # with open('/data.json') as f:
+    #     data = json.load(f)
+    
+    
+    # return jsonify(data)
+
+
+    return send_file('data.json', mimetype='application/json')
+
+
+
+    
+
+
+    
