9420964e1a36eb2dfc0b65f1c9860d26b198129f
gautamkumar30 <gautamkumarganesan@gmail.com>
2024-10-06 14:03:55 +0530

    enhance

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <main className="flex flex-col w-full min-h-screen py-20 px-10 gap-10">
-      <h1 className="text-4xl font-bold text-center">GoDomain SIH</h1>
+      <h1 className="text-4xl font-bold text-center">
+        GoDomain SIH - Pixel Pioneers
+      </h1>
       <InputSearch />
     </main>
   );
